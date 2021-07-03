26aecdd88b140d4695a17d2a79b7f7477483e4c5
pedrooliver13 <pedro@qyon.com>
2021-07-03 19:06:19 -0300

    add api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7351ff7ac371226d95d16493e41e5800'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dae162fa-9743-4827-b204-185adb201749'
+};
